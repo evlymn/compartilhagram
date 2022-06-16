5a4d188bee4523eead4c9e2db6d7c8fbd7d90b1a
Evelyn Mendes <evelynmendes@bemol.com.br>
2022-06-16 18:01:59 -0300

    modulo e component profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/component/login/login.component';
+import { ProfileComponent } from './profile/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
